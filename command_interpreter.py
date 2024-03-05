ef45f72100e58aeaf6977fb950110e86e5bf34ac
henrysg1 <henrysandsgrant@gmail.com>
2024-03-05 12:40:27 +0000

    Fixing repeating packets

--- command_interpreter.py
+++ command_interpreter.py
@@ -1,7 +1,16 @@
 import struct
+import socket
+import time
+import random
 
-def binary(num):
+HOST = "localhost"
+
+def float_to_binary_32(num):
     return ''.join('{:0>8b}'.format(c) for c in struct.pack('!f', num))
+
+#ST3_TM_DICT = {
+#    (3, 25): { "name": "TM(3,25)_report_housekeeping_parameter_report_structure", "send_interval": 1, "data": Housekeeping_ADCS_0_01 },
+#}
 
 def hex_to_command(data):
     # Dictionary mapping (service_type, service_subtype) to command names
@@ -61,59 +70,204 @@ def hex_to_command(data):
 
     return command_name
 
-def send_telemetry():
-    
-    # Define CCSDS header
+def send_telemetry(simulator):
 
+    SEQUENTIAL_SENDING = False
+
+    portOBC = 10015
+    tm_socket_OBC = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tm_socket_OBC.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    portADCS = 10016
+    tm_socket_ADCS = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tm_socket_ADCS.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    # portCAN = 10017
+    # tm_socket_CAN = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # tm_socket_CAN.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    tm_socket_OBC.bind((HOST, portOBC))
+    tm_socket_OBC.listen(1)
+    print("\nserver "+str(portOBC)+" listening")
+
+    tm_socket_ADCS.bind((HOST, portADCS))
+    tm_socket_ADCS.listen(1)
+    print("server "+str(portADCS)+" listening")
+
+    # tm_socket_CAN.bind((HOST, portCAN))
+    # tm_socket_CAN.listen(1)
+    # print("server  10017 listening")
+
+    clientconnOBC, _ = tm_socket_OBC.accept()
+
+    clientconnADCS, _ = tm_socket_ADCS.accept()
+
+    # clientconnCAN, _ = tm_socket_CAN.accept()
+
+    packetCounter = 0
+    simulator.tm_counter = 0
+    
+    last_obc_send_time = time.time() - 3  # Ensures an OBC packet is sent on the first iteration
+
+    packetCounter = 0
+
+    while packetCounter < 400:
+        current_time = time.time()
+
+        # Determine which packet to send
+        if current_time - last_obc_send_time >= 3:
+            # Time to send Housekeeping_OBC_3
+            data = Housekeeping_OBC_3()
+            last_obc_send_time = current_time
+        else:
+            # Send Housekeeping_ADCS_0_01
+            data = Housekeeping_ADCS_0_01()
+
+        # Construct and send the telemetry packet
+        tm_secondary_header = create_tm_secondary_header(3, 25)
+        ccsds_header = create_ccsds_header(data, tm_secondary_header)
+        packet = combine_packet_information(ccsds_header, tm_secondary_header, data)
+        
+        acceptance = acceptance_packets()
+        confirmation = confirmation_packets()
+
+        # for binary_packet in acceptance:
+
+        #     expected_hex_length = (len(binary_packet) // 8) * 2
+
+        #     tm_packet = hex(int(binary_packet, 2))[2:]
+
+        #     tm_packet_hex_padded = tm_packet.zfill(expected_hex_length)
+
+        #     packet = bytes.fromhex(tm_packet_hex_padded)
+
+        #     clientconnOBC.send(packet)
+        #     clientconnADCS.send(packet)
+        #     simulator.tm_counter += 1
+        #     packetCounter += 1
+
+        clientconnOBC.send(packet)
+        clientconnADCS.send(packet)
+        simulator.tm_counter += 1
+
+        packetCounter += 1
+
+        # for binary_packet in confirmation:
+
+        #     expected_hex_length = (len(binary_packet) // 8) * 2
+
+        #     tm_packet = hex(int(binary_packet, 2))[2:]
+
+        #     tm_packet_hex_padded = tm_packet.zfill(expected_hex_length)
+
+        #     packet = bytes.fromhex(tm_packet_hex_padded)
+
+        #     clientconnOBC.send(packet)
+        #     clientconnADCS.send(packet)
+        #     simulator.tm_counter += 1
+        #     packetCounter += 1
+
+        # Adjust sleep to ensure timing accuracy, considering execution time
+        sleep_time = 1 - (time.time() - current_time)
+        if sleep_time > 0:
+            time.sleep(sleep_time)
+
+    clientconnOBC.close()
+    clientconnADCS.close()
+    # clientconnCAN.close()
+    print("communication ended")
+
+    return
+
+def create_ccsds_header(packet_data, tm_secondary_header):
     packet_version_number = '000'
     packet_type = '0'
     secondary_header_flag = '1'
     apid = '00000000001'
     sequence_flags = '11'
     packet_name = '00000000000010'
+    packet_data_length = format(((len(packet_data) + len(tm_secondary_header)) // 8) - 1, 'b').zfill(16)
 
-    # To be determined from data
-    packet_data_length = '0000000000000000'
+    ccsds_header = packet_version_number + packet_type + secondary_header_flag + apid + sequence_flags + packet_name + packet_data_length
 
-    # Define TM packet secondary header
+    return ccsds_header
 
+def create_tm_secondary_header(service_type, service_subtype):
     tm_packet_pus_version_number = '0010'
     spacecraft_time_reference_status = '0000'
-    service_type = '00000011'
-    service_subtype = '00011001'
+    service_type = f'{service_type:08b}'
+    service_subtype = f'{service_subtype:08b}'
     message_type_counter = '0000000000000000'
     destination_ID = '0000000000000001'
-    time = '00001111001010111101000101101100'
 
-    enum_type = '00000011'
+    # Get the current time in seconds since the epoch
+    current_time_seconds = int(time.time())
 
-    adcs_magnetometer_raw_x = binary(5.4)
-    adcs_magnetometer_raw_y = binary(6.4)
-    adcs_magnetometer_raw_z = binary(7.4)
-    adcs_gyroscope_x = binary(8.4)
-    adcs_gyroscope_y = binary(9.4)
-    adcs_gyroscope_z = binary(10.4)
+    # Convert to a binary string
+    send_time = format(current_time_seconds, '032b')
 
-    # Calculate the length of the bit string
+    tm_secondary_header = tm_packet_pus_version_number + spacecraft_time_reference_status + service_type + service_subtype + message_type_counter + destination_ID + send_time
 
-    bit_length = len(tm_packet_pus_version_number + spacecraft_time_reference_status + service_type + service_subtype + message_type_counter + destination_ID + time
-                        + enum_type + adcs_magnetometer_raw_x + adcs_magnetometer_raw_y + adcs_magnetometer_raw_z + adcs_gyroscope_x + adcs_gyroscope_y + adcs_gyroscope_z)
-    
-    # Calculate the number of bytes
-    packet_data_length = format(bit_length // 8, 'b').zfill(16)
+    return tm_secondary_header
 
-    packet_bits = packet_version_number + packet_type + secondary_header_flag + apid + sequence_flags + packet_name + packet_data_length
-    packet_bits += tm_packet_pus_version_number + spacecraft_time_reference_status + service_type + service_subtype + message_type_counter + destination_ID + time
-    packet_bits += enum_type + adcs_magnetometer_raw_x + adcs_magnetometer_raw_y + adcs_magnetometer_raw_z + adcs_gyroscope_x + adcs_gyroscope_y + adcs_gyroscope_z
+def combine_packet_information(ccsds_header, tm_secondary_header, packet_data):
+    packet_bits = ccsds_header + tm_secondary_header + packet_data
 
     expected_hex_length = (len(packet_bits) // 8) * 2
 
-    tm_packet = hex(int(packet_version_number + packet_type + secondary_header_flag + apid + sequence_flags + packet_name + packet_data_length
-                        + tm_packet_pus_version_number + spacecraft_time_reference_status + service_type + service_subtype + message_type_counter + destination_ID + time
-                        + enum_type + adcs_magnetometer_raw_x + adcs_magnetometer_raw_y + adcs_magnetometer_raw_z + adcs_gyroscope_x + adcs_gyroscope_y + adcs_gyroscope_z, 2))[2:]
+    tm_packet = hex(int(packet_bits, 2))[2:]
 
     tm_packet_hex_padded = tm_packet.zfill(expected_hex_length)
 
     tm_packet_bytes = bytes.fromhex(tm_packet_hex_padded)
 
     return tm_packet_bytes
+
+def Housekeeping_ADCS_0_01():
+    # Create data for the TM packet    
+    enum_type = '00000011'
+
+    adcs_magnetometer_raw_x = format(random.randint(0, 100), '08b')
+    adcs_magnetometer_raw_y = format(random.randint(0, 100), '08b')
+    adcs_magnetometer_raw_z = format(random.randint(0, 100), '08b')
+    adcs_gyroscope_x = format(random.randint(0, 1000000), '032b')
+    adcs_gyroscope_y = format(random.randint(0, 1000000), '032b')
+    adcs_gyroscope_z = format(random.randint(0, 1000000), '032b')
+
+    data = enum_type + adcs_magnetometer_raw_x + adcs_magnetometer_raw_y + adcs_magnetometer_raw_z + adcs_gyroscope_x + adcs_gyroscope_y + adcs_gyroscope_z
+
+    return data
+
+def Housekeeping_OBC_3():
+    enum_type = '00000001'
+
+    obc_pcb_board_temperature_1 = float_to_binary_32(random.uniform(18, 35))
+    obc_pcb_board_temperature_2 = float_to_binary_32(random.uniform(18, 35))
+    obc_spacecraft_time_ref = '00000000'
+    obc_operational_mode = '00000001'
+    obc_memory_partition = '00000001'
+    obc_reconfiguration_timer = '00000000000000000000000011111111'
+    obc_last_failed_event = '0101010101010101'
+    obc_mcu_sys_tick = '00000000000000000000111111111111'
+
+    data = enum_type + obc_pcb_board_temperature_1 + obc_pcb_board_temperature_2 + obc_spacecraft_time_ref + obc_operational_mode + obc_memory_partition + obc_reconfiguration_timer + obc_last_failed_event + obc_mcu_sys_tick
+
+    return data
+
+def acceptance_packets():
+    return [
+        '000010000000000111000000000010110000000000001110001000000000000100000001000000000000000000000000000000010000111100101011110100011101000000011000000000111100000000000000',
+        '0000100000000001110000000000110000000000000100000010000000000001000000100000000000000000000000000000000100001111001010111101000111010000000110000000001111000000000000000000000000000000'
+    ]
+
+def confirmation_packets():
+
+    return [
+        '000010000000000111000000000011010000000000001110001000000000000100000011000000000000000000000000000000010000111100101011110100011101000000011000000000111100000000000000',
+        '0000100000000001110000000000001100000000000100000010000000000001000001000000000000000000000000000000000100001111001010111101000110011110000110000000000111000000000000000000000000001000',
+        '00001000000000011100000000001111000000000000111100100000000000010000010100000000000000000000000000000001000011110010101111010001110100000001100000000011110000000000000000000000',
+        '000010000000000111000000000100000000000000010001001000000000000100000110000000000000000000000000000000010000111100101011110100011101000000011000000000111100000000000000000000000000000000000000',
+        '000010000000000111000000000100010000000000001110001000000000000100000111000000000000000000000000000000010000111100101011110100011101000000011000000000111100000000000000',
+        '0000100000000001110000000001001000000000000100000010000000000001000010000000000000000000000000000000000100001111001010111101000111010000000110000000001111000000000000000000000000000000',
+        '0000100000000001110000000001001100000000000100000010000000000001000010100000000000000000000000000000000100001111001010111101000111010000000110000000001111000000000000000000000000000000',
+    ]
--- src/main/java/com/uoncubesat/MyPacketPreprocessor.java
+++ src/main/java/com/uoncubesat/MyPacketPreprocessor.java
@@ -4,34 +4,19 @@ import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.*;
 
 import org.yamcs.TmPacket;
 import org.yamcs.YConfiguration;
 import org.yamcs.tctm.AbstractPacketPreprocessor;
-import org.yamcs.utils.TimeEncoding;
 
-/**
- * Component capable of modifying packet binary received from a link, before passing it further into Yamcs.
- * <p>
- * A single instance of this class is created, scoped to the link udp-in.
- * <p>
- * This is specified in the configuration file yamcs.UoNCubeSat.yaml:
- * 
- * <pre>
- * ...
- * dataLinks:
- *   - name: udp-in
- *     class: org.yamcs.tctm.UdpTmDataLink
- *     stream: tm_realtime
- *     host: localhost
- *     port: 10015
- *     packetPreprocessorClassName: com.uoncubes.MyPacketPreprocessor
- * ...
- * </pre>
- */
+import com.uoncubesat.file_handling.network.in.TMParser;
+
 public class MyPacketPreprocessor extends AbstractPacketPreprocessor {
+    private static final Logger LOGGER = Logger.getLogger(MyPacketPreprocessor.class.getName());
 
-    private Map<Integer, AtomicInteger> seqCounts = new HashMap<>();
+    private final TMParser listener = new TMParser();
+    private final Map<Integer, AtomicInteger> seqCounts = new HashMap<>();
 
     // Constructor used when this preprocessor is used without YAML configuration
     public MyPacketPreprocessor(String yamcsInstance) {
@@ -47,35 +32,94 @@ public class MyPacketPreprocessor extends AbstractPacketPreprocessor {
     @Override
     public TmPacket process(TmPacket packet) {
 
-        byte[] bytes = packet.getPacket();
-        if (bytes.length < 6) { // Expect at least the length of CCSDS primary header
-            eventProducer.sendWarning("SHORT_PACKET",
-                    "Short packet received, length: " + bytes.length + "; minimum required length is 6 bytes.");
+        LOGGER.info("In process".toString());
 
+        byte[] bytes = packet.getPacket();
+
+        // Expect at least the length of CCSDS primary and secondary header
+        if (bytes.length < 17) {
+            eventProducer.sendWarning("SHORT_PACKET",
+                    "Short packet received, length: " + bytes.length + "; minimum required length is 17 bytes.");
             // If we return null, the packet is dropped.
             return null;
         }
 
         // Verify continuity for a given APID based on the CCSDS sequence counter
-        int apidseqcount = ByteBuffer.wrap(bytes).getInt(0);
-        int apid = (apidseqcount >> 16) & 0x07FF;
-        int seq = (apidseqcount) & 0x3FFF;
-        AtomicInteger ai = seqCounts.computeIfAbsent(apid, k -> new AtomicInteger());
-        int oldseq = ai.getAndSet(seq);
+        int apidseqcount = ByteBuffer.wrap(bytes).getInt(0); // first 4 bytes (0-3)
+        int datalength = ByteBuffer.wrap(bytes).getShort(4) + 1; // get 2 bytes (4-5)
+        int apid = (apidseqcount >> 16) & 0x07FF; // 11 bits 
+        int seqcount = (apidseqcount) & 0x3FFF; // 14 bits
+        int packversion = (apidseqcount >> 29) & 0x7; // 3 bits
+        int secheader = (apidseqcount >> 27) & 0x1; // 1 bit
+        int pusversion = ByteBuffer.wrap(bytes).get(6) & 0xF;// 4 bits
+        int serviceType = ByteBuffer.wrap(bytes).get(7);    //8 bits
+        int messageType = ByteBuffer.wrap(bytes).get(8);    //8 bits
+        short messageTypeCounter = ByteBuffer.wrap(bytes).getShort(9); //get 2 bytes (9-10)
+        short destinationID = ByteBuffer.wrap(bytes).getShort(11); // get 2 bytes (11-12)
+        long time = ByteBuffer.wrap(bytes).getInt(13); // 32 bits. Long to prevent overflow
 
-        if (((seq - oldseq) & 0x3FFF) != 1) {
+        AtomicInteger ai = seqCounts.computeIfAbsent(apid, k -> new AtomicInteger());
+        int oldseq = ai.getAndSet(seqcount);
+        StringBuilder stringBuilder = new StringBuilder();
+        String newline = System.getProperty("line.separator");
+        stringBuilder.append("New packet received!").append(newline);
+        if (((seqcount - oldseq) & 0x3FFF) != 2) {
+            stringBuilder.append("Sequence count jump for APID: ").append(apid).append(" old seq: ").append(oldseq).append(" newseq: ").append(seqcount)
+                    .append(newline);
             eventProducer.sendWarning("SEQ_COUNT_JUMP",
-                    "Sequence count jump for APID: " + apid + " old seq: " + oldseq + " newseq: " + seq);
+                    "Sequence count jump for APID: " + apid + " old seq: " + oldseq + " newseq: " + seqcount);
         }
 
+        if (packversion != 0) {
+            stringBuilder.append("Wrong version number. Expected 0 and got ").append(packversion).append(newline);
+            eventProducer.sendWarning("PACKET_VERSION_ERROR",
+                    "Wrong version number. Expected 0 and got " + packversion);
+
+        }
+
+        if (secheader != 1) {
+            stringBuilder.append("Wrong secondary flag. Expected 1 and got ").append(secheader).append(newline);
+            eventProducer.sendWarning("SEC_HEADER_FLAG_ERROR",
+                    "Wrong secondary flag. Expected 1 and got " + secheader);
+        }
+
+        if (datalength != (bytes.length - 6)) {
+            stringBuilder.append("Wrong packet data length. Expected ").append(bytes.length - 6).append(" and got ")
+                    .append(datalength).append(newline);
+            eventProducer.sendWarning("PACKET_LENGTH_ERROR",
+                    "Wrong packet data length. Expected " + (bytes.length - 6) + " and got "
+                            + datalength);
+        }
+        stringBuilder.append("Sequence_count:").append(seqcount).append(newline);
+        stringBuilder.append("APID:").append(apid).append(newline);
+        stringBuilder.append("PUS:").append(pusversion).append(newline);
+        stringBuilder.append("Secondary_header:").append(secheader).append(newline);
+        stringBuilder.append("Buffer:").append(apidseqcount).append(newline);
+        stringBuilder.append("Time:").append(time).append(newline);
+        stringBuilder.append("ServiceType:").append(serviceType).append(newline);
+        stringBuilder.append("MessageType:").append(messageType).append(newline);
+        stringBuilder.append("Packet data length:").append(datalength).append(newline);
+        stringBuilder.append("----").append(newline);
+        LOGGER.info(stringBuilder.toString());
         // Our custom packets don't include a secundary header with time information.
         // Use Yamcs-local time instead.
-        packet.setGenerationTime(TimeEncoding.getWallclockTime());
+
 
         // Use the full 32-bits, so that both APID and the count are included.
-        // Yamcs uses this attribute to uniquely identify the packet (together with the gentime)
-        packet.setSequenceCount(apidseqcount);
+        // Yamcs uses this attribute to uniquely identify the packet (together with the
+        // gentime)
+        // packet.setSequenceCount(apidseqcount);
+        if(serviceType == 6 && messageType == 4)
+            listener.parseFileSegmentPacket(packet.getPacket());
+        packet.setGenerationTime(System.currentTimeMillis());
 
         return packet;
+
+    }
+
+    // Returns the unix timestamp in milliseconds
+    long CUCtoUnix(long time) {
+        long start = 1577836800; // 1/1/2020 00:00:00 GMT +0200
+        return start * 1000 + time * 100;
     }
 }
