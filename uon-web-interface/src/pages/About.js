d63961633452c823c1a88f47c2426cc893f7b163
henrysg1 <henrysandsgrant@gmail.com>
2024-04-03 12:43:27 +0100

    Updating page format

--- uon-web-interface/src/pages/About.js
+++ uon-web-interface/src/pages/About.js
@@ -3,11 +3,11 @@ import React from 'react';
 function About() {
   return (
     <div style={{ padding: '20px' }}>
-      <h1>Welcome to Mein</h1>
-      <p>This is the home page of our awesome React application.</p>
+      <h1>Welcome to the UoN CubeSat Website</h1>
+      <p>This is an example page of the React application.</p>
       
       {/* Example of adding an image */}
-      <img src="url-to-your-image.jpg" alt="Descriptive Alt Text" style={{ maxWidth: '100%', height: 'auto' }} />
+      <img src="../images/logo.png" alt="Descriptive Alt Text" style={{ maxWidth: '100%', height: 'auto' }} />
 
       {/* Additional content */}
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
